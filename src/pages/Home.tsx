90e21f57b3bc0fc72bc30ebaf69522eb5139d946
YCrhis <htcrhis@gmail.com>
2022-03-10 19:37:16 -0500

    fixing reload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from "react"
 import { Banner } from "../components/Banner"
 
 export const Home = () => {
-
-    window.localStorage.setItem('hola', JSON.stringify({ name: 'crhis' }))
-
-    useEffect(() => {
-        window.location.reload()
-    }, [])
-
-
     return (
         <div>
             <Banner />
